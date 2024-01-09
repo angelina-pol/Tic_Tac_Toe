2a06d1e3716f317180e335efb72c1fa78acd95c8
Angelina <gela-turlova@inbox.ru>
2024-01-09 12:03:16 +0300

    added game board

---
+++ src/Square.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css';
+import { useState } from 'react';
+
+export default function Square() {
+  const [value, setValue] = useState<string | null>(null)
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return <button 
+            className='square'
+            onClick={handleClick}
+          >
+            {value}
+          </button>
+}
